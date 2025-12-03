c9822bbf9bfbb360ddae066467311bb3959a2559
lewtwelf <lewtwelf@gmail.com>
2025-12-03 10:23:54 +0000

    hello world

--- TFL_Streaming/src/stream_archive.py
+++ TFL_Streaming/src/stream_archive.py
@@ -1,21 +1,33 @@
 # -*- coding: utf-8 -*-
 from pyspark.sql import SparkSession
+import yaml
+import os
 
-def create_spark_session(app_name="UK_TFL_ARCHIVE_INCOMING"):
-    """Create and return a SparkSession."""
-    return SparkSession.builder.appName(app_name).getOrCreate()
+with open("config/dev.yaml") as f:
+    cfg = yaml.safe_load(f)
+
+KAFKA_SERVER = cfg["kafka"]["bootstrap_servers"]
+TOPIC = cfg["kafka"]["topic"]
+POLL_INTERVAL = cfg["tfl"]["polling_interval"]
+
+TFL_APP_ID = os.getenv("TFL_APP_ID")
+TFL_APP_KEY = os.getenv("TFL_APP_KEY")
+API_LIST = cfg["tfl"]["api_list"]
+CHECKPOINT_PATH = cfg["hdfs"]["checkpoint_path"]
+INCOMING_PATH = cfg["hdfs"]["incoming_path"]
+
+spark = SparkSession.builder.appName(TFL_APP_ID).getOrCreate()
 
 def get_hadoop_fs(spark):
     """Return Hadoop FileSystem object from SparkSession."""
     return spark._jvm.org.apache.hadoop.fs.FileSystem.get(spark._jsc.hadoopConfiguration())
 
-def archive_files(fs, src_path, dst_path):
-    """Move all files from src_path to dst_path in HDFS."""
+def archive_files(fs, src_path, dst_path, spark):
     if fs.exists(src_path):
         files = fs.listStatus(src_path)
         for f in files:
             src = f.getPath()
-            dst = spark._jvm.org.apache.hadoop.fs.Path(f"{dst_path}/{src.getName()}")
+            dst = spark._jvm.org.apache.hadoop.fs.Path("{}/{}".format(dst_path, src.getName()))
             fs.rename(src, dst)
         print("Archived incoming parquet files")
 
@@ -30,10 +42,8 @@ def main():
     incoming_path = "hdfs:///tmp/DE011025/uk/streaming/incoming/"
     archive_path = "/tmp/DE011025/uk/streaming/archive"
 
-    spark = create_spark_session()
     fs = get_hadoop_fs(spark)
     hadoop_incoming = spark._jvm.org.apache.hadoop.fs.Path(incoming_path)
-    hadoop_archive = spark._jvm.org.apache.hadoop.fs.Path(archive_path)
 
     archive_files(fs, hadoop_incoming, archive_path)
     delete_remaining(fs, hadoop_incoming)
